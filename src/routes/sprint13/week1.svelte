f3f00c2c054830edf7a6eb0a17b89ff2d4c4200f
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-12 17:58:00 +0200

    remove:dit heb ik geleerd week 1 sprint 13 is removed the text is double

--- src/routes/sprint13/week1.svelte
+++ src/routes/sprint13/week1.svelte
@@ -8,7 +8,6 @@
 
 <h2 id="week1">week1</h2>
 <ul>
-    <li>ga naar <a href="#geleerd1">wat heb ik geleerd</a></li>
     <li>ga naar <a href="#gedaan1">wat heb ik gedaan</a></li>
 </ul>
 
@@ -27,41 +26,7 @@
                      npm run dev
                     npm install @directus/sdk
                 </code></pre>
-
-
-    <p><strong>3-9</strong>
-        Ik heb verder de tutorial gevolgd en
-        ik heb gekeken naar hoe ik mijn project kan deployen op vercel en dat is gleukt dankzij deze link <a
-                href="https://vercel.com/docs/frameworks/sveltekit">https://vercel.com/docs/frameworks/sveltekit</a>
-    </p>
-
-
-    <p><strong>4-9</strong> ik heb verder de tutorial gevolgd en een schets gemaakt voor mijn profilecard en ik heb op
-        vercel deze
-        tutorial
-        gevonden en dit heeft mij enorm geholpen in het begrijpen van sveltekit
-        <a href="https://vercel.com/docs/beginner-sveltekit">https://vercel.com/docs/beginner-sveltekit</a>
-
-        <img src="{logo}">
-    </p>
-
-
-
-    <p><strong>5-9</strong> ik heb mijn profilecard in code uitgewerkt en het button component gemaakt voor de squad
-        page en dit is
-        gelukt
-        met behulp van de tutorial
-        <br>
-        <img src="https://raw.githubusercontent.com/yujing-student/i-love-web/main/img.png">
-
-        <br>
-        <strong>6-9</strong> ik heb geleerd hoe ik netjes issues kan inschieten en feedback kan geven waardoor
-        eerstejaars beter
-        begrijpen
-        hoe ze naar de oplossing kunnnen werken</p>
-
-    <h3 id="geleerd1">dit heb ik geleerd</h3>
-
+    <img src="{andere}">
 
     <h4>svelte</h4>
     <p>dit zijn aantekingen over svelte en sveltekit</p>
@@ -81,22 +46,25 @@
         minimaal
         is.</p>
 
-    <h4>code</h4>
-    <p>installeren svelte</p>
-    <pre><code class="">
-                    npm create svelte@latest
-                     npm install
-                     npm run dev
-                    npm install @directus/sdk
-                </code></pre>
-    <p>ophalen data uit de databse
 
+
+    <p><strong>3-9</strong>
+        Ik heb verder de tutorial gevolgd en
+        ik heb gekeken naar hoe ik mijn project kan deployen op vercel en dat is gleukt dankzij deze link <a
+                href="https://vercel.com/docs/frameworks/sveltekit">https://vercel.com/docs/frameworks/sveltekit</a>
+    </p>
+
+
+    <p><strong>4-9</strong> ik heb verder de tutorial gevolgd en een schets gemaakt voor mijn profilecard en ik heb op
+        vercel deze
+        tutorial
+        gevonden en dit heeft mij enorm geholpen in het begrijpen van sveltekit
+        <a href="https://vercel.com/docs/beginner-sveltekit">https://vercel.com/docs/beginner-sveltekit</a>
+
+        <img src="{logo}">
 
 
     </p>
-
-    <img src="{andere}">
-
 
     <h4>sveltekit</h4>
     <p>
@@ -110,5 +78,21 @@
         <br>
         Code Splitting: Code wordt slim verdeeld over client en server
     </p>
+
+
+
+    <p><strong>5-9</strong> ik heb mijn profilecard in code uitgewerkt en het button component gemaakt voor de squad
+        page en dit is
+        gelukt
+        met behulp van de tutorial
+        <br>
+        <img src="https://raw.githubusercontent.com/yujing-student/i-love-web/main/img.png">
+
+        <br>
+        <strong>6-9</strong> ik heb geleerd hoe ik netjes issues kan inschieten en feedback kan geven waardoor
+        eerstejaars beter
+        begrijpen
+        hoe ze naar de oplossing kunnnen werken</p>
+
 
 </section>
