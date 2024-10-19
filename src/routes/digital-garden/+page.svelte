217a274a4790e4c49aaabe5a23a9ea18d659a15d
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-19 18:31:52 +0200

    learning journal has styling

---
+++ src/routes/digital-garden/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+<main class="green-gradient">
+    <section class="generalStyling ">
+        <h1 id="mijn-leervragen">digital garden</h1>
+        <p>hier kunt u all mijn schoolwerk zien
+        </p>
+
+
+    </section>
+</main>
+<style>
+    main {
+        padding: 2em;
+        display: grid;
+        gap: 2em;
+        font-family: Arial, Helvetica, sans-serif;
+        scroll-behavior: smooth;
+
+
+    }
+
+
+</style>
--- src/routes/learning-journal/+page.svelte
+++ src/routes/learning-journal/+page.svelte
@@ -2,15 +2,22 @@
 
 </script>
 <main class="green-gradient">
-    <section class="generalStyling ">
+    <section class="general-styling-section ">
         <h1 id="mijn-leervragen">Learning-journal</h1>
         <p>mijn learning journal gaat over wat ik iedere week leer en dit hou ik bij in een markdown
-            omdat dit overzichtelijker is
-
-
+            omdat dit overzichtelijker is kies de sprint uit en klik dan welke week je wilt zien
         </p>
-        <p><a href="#">link de map met daarin het learning journal als markdown
-        </a> </p>
+        <ul>
+            <li>
+                <a href="https://github.com/yujing-student/i-love-web/tree/main/learning-journal/sprint13">sprint 13</a>
+            </li>
+            <li>
+                <a href="https://github.com/yujing-student/i-love-web/tree/main/learning-journal/sprint14">sprint 14</a>
+            </li>
+            <li>
+                <a href="https://github.com/yujing-student/i-love-web/tree/main/learning-journal/sprint15">sprint 15</a>
+            </li>
+        </ul>
 
 
     </section>
