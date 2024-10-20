ca0a81933784e56f4e3548c35aaf17aa3785ae62
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-20 11:51:49 +0200

    i have added the cards there is only missing the right images and title

--- src/routes/digital-garden/+page.svelte
+++ src/routes/digital-garden/+page.svelte
@@ -1,31 +1,234 @@
 <script>
+    let projects = [
+        {
+            title: "Your tribe",
+            period: "2023",
+            year: "year 1",
+            programming_language: "html,css",
+            semester:'semester 1',
+        }, {
+            title: "The client",
+            period: "2023",
+            year: "year 1",
+            programming_language: "html,css",
+            semester:'semester 1',
+        }, {
+            title: "Look and feel",
+            period: "2023",
+            year: "year 1",
+            programming_language: "html,css",
+            semester:'semester 1',
+        },
+        {
+            title: "fix the flow",
+            period: "2023",
+            year: "year 1",
+            programming_language: "html,css, javascript",
+            semester:'semester 1',
+        },
+        {
+            title: "The startup",
+            period: "2023",
+            year: "year 1",
+            programming_language: "html,css, javascript"
+            ,
+            semester:'semester 1',
+        },
 
+
+        {
+            title: "connect your tibe",
+            period: "2024",
+            year: "year 1",
+            programming_language: "html,css, javascript,node js,express",
+            semester:'semester 2',
+        },
+
+        {
+            title: "serverside side rendering",
+            period: "2024",
+            year: "year 2",
+            programming_language: "html,css, javascript,node js,express",
+            semester:'semester 2',
+        },
+
+    ];
 </script>
 
 
-<section class="general-styling-section ">
+<section class="general-styling-section  ">
     <h1>digital garden</h1>
     <p>
         hier kunt u all mijn schoolwerk zien
     </p>
+    <div class="grid-container">
 
 
+        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
 
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>
+        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>        <article class="card">
+            <picture>
+                <source src="/img/dda-website.png">
+                <img src="/img/dda-website.png">
+
+            </picture>
+
+            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
+            </h2>
+
+        </article>
+    </div>
 </section>
 
 
-
-<section class="general-styling-section">
-
-</section>
 <style>
-    h1{
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(263px, 1fr));
+        text-transform: uppercase;
+        grid-row-gap: 1rem;
+    }
+
+    .card {
+        width: 150px;
+        overflow: hidden;
+        padding: 2rem;
+        display: grid;
+        gap: 16px;
+        background-color: #00ff92;
+    }
+
+    img {
+        object-fit: cover;
+        width: 150px;
+    }
+
+    h1 {
 
     }
-    h2{
+
+    h2 {
         /*border-bottom: solid 2px var(--purple);*/
     }
-
 
 
 </style>
