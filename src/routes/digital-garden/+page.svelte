a3c5098a90e4fc2264b8c06aee89a16d72c80d2e
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-20 12:50:59 +0200

    i have added the cards there is only missing the images and styling the data is from a object

--- src/routes/digital-garden/+page.svelte
+++ src/routes/digital-garden/+page.svelte
@@ -1,55 +1,123 @@
 <script>
-    let projects = [
+    let projects_year1_semester1 = [
         {
-            title: "Your tribe",
+            title: "Your profile card",
             period: "2023",
-            year: "year 1",
+            year: "jaar 1",
             programming_language: "html,css",
             semester:'semester 1',
-        }, {
+            link_project:'https://github.com/yujing-student/your-tribe-profile-card',
+            img:'',
+        },
+        {
+            title: "Your tribe squad page",
+            period: "2023",
+            year: "jaar 1",
+            programming_language: "html,css",
+            semester:'semester 1',
+            link_project:'https://github.com/yujing-student/your-tribe-squad-page',
+            img:'',
+        },
+
+        {
             title: "The client",
             period: "2023",
-            year: "year 1",
+            year: "jaar 1",
             programming_language: "html,css",
             semester:'semester 1',
+            link_project:'https://github.com/yujing-student/the-client-website',
+            img:'',
         }, {
             title: "Look and feel",
             period: "2023",
-            year: "year 1",
+            year: "jaar 1",
             programming_language: "html,css",
             semester:'semester 1',
+            link_project:'https://github.com/yujing-student/look-and-feel-corporate-identity',
+            img:''
         },
         {
             title: "fix the flow",
             period: "2023",
-            year: "year 1",
+            year: "jaar 1",
             programming_language: "html,css, javascript",
             semester:'semester 1',
+            img:'',
+            link_project: 'https://github.com/yujing-student/fix-the-flow-interactive-website'
         },
         {
             title: "The startup",
             period: "2023",
-            year: "year 1",
-            programming_language: "html,css, javascript"
-            ,
+            year: "jaar 1",
+            programming_language: "html,css, javascript",
+            img:'',
             semester:'semester 1',
+            link_project: 'https://github.com/yujing-student/the-startup-responsive-interactieve-website'
         },
 
+
+
+    ];
+
+
+    let projects_year1_semester2 = [
 
         {
             title: "connect your tibe",
             period: "2024",
-            year: "year 1",
+            year: "jaar 1",
             programming_language: "html,css, javascript,node js,express",
             semester:'semester 2',
+            img:''
         },
 
         {
             title: "serverside side rendering",
             period: "2024",
-            year: "year 2",
+            year: "jaar 2",
             programming_language: "html,css, javascript,node js,express",
             semester:'semester 2',
+            img:''
+        },
+
+
+    ];
+
+    let smallproject = [
+        {
+            title: "css layout",
+            period: "2023",
+            year: "jaar 1",
+            programming_language: "html,css, javascript,node js,express",
+            semester:'semester 1',
+        }
+    ]
+
+
+    let projects_year2_semester1 = [
+        {
+            title: "your tribe for life profile card",
+            period: "2023",
+            year: "jaar 1",
+            programming_language: "sveltekit",
+            semester:'semester 1',
+            link_project:'https://github.com/yujing-student/your-tribe-for-life-profile-card',
+            img:'/img/dda-website.png'
+        },
+        {
+            title: "Your tribe squad page",
+            programming_language: "sveltekit",
+            semester:'semester 1',
+            link_project:'https://github.com/KaanKalmi/your-tribe-for-life-squad-page',
+        },
+
+
+        {
+            title: "choices-choices-the-tech-stack",
+
+            programming_language: "eleventy",
+            semester:'semester 1',
+            link_project:'https://github.com/yujing-student/choices-choices-the-tech-stack',
         },
 
     ];
@@ -59,153 +127,64 @@
 <section class="general-styling-section  ">
     <h1>digital garden</h1>
     <p>
-        hier kunt u all mijn schoolwerk zien
+        hier kunt u all mijn schoolwerk zien met hier de hoofdprojecten
     </p>
+
+
+    <h2>schoolprojecten dit jaar</h2>
+    <p>Dit jaar ben ik bezig met frameworks om hier beter in te wordem</p>
     <div class="grid-container">
+        {#each projects_year2_semester1 as member}
+            <article class="card">
+                <picture>
+                    <source src="{member.img}">
+                    <img src="{member.img}">
+
+                </picture>
+            <h3 aria-label="project {member.title}"><a href="{member.link_project}">
+                {member.title}
+            </a> </h3>
+            <ul>
+                <li>{member.programming_language}</li>
+            </ul>
+            </article>
+        {/each}
+    </div>
 
 
-        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
 
-            </picture>
+    <h2>schoolprojecten 2023</h2>
+    <p>Dit jaar heb ik html en css geleerd en ook een beetje javascript</p>
+    <div class="grid-container">
+        {#each projects_year1_semester1 as member}
+            <article class="card">
+                <picture>
+                    <source src="{member.img}">
+                    <img src="{member.img}">
 
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>
-        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>        <article class="card">
-            <picture>
-                <source src="/img/dda-website.png">
-                <img src="/img/dda-website.png">
-
-            </picture>
-
-            <h2 aria-label="bedrijfsnaam"><a href="choices-choices-the-tech-stack">choices-choices-the-tech-stack</a>
-            </h2>
-
-        </article>
+                </picture>
+                <h3 aria-label="project {member.title}"><a href="{member.link_project}">
+                    {member.title}
+                </a> </h3>
+                <ul>
+                    <li>{member.programming_language}</li>
+                </ul>
+            </article>
+        {/each}
     </div>
 </section>
-
 
 <style>
     .grid-container {
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(263px, 1fr));
-        text-transform: uppercase;
         grid-row-gap: 1rem;
+    }
+    ul{
+        padding: 0;
+    }
+    li{
+        list-style: none;
     }
 
     .card {
