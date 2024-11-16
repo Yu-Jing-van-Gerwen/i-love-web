f630a459b29e3f7eb0abe8948d12c7966d8ed0a8
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-16 14:29:46 +0100

    change color background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,10 +14,7 @@
 
 
     body{
-        background: #00ff92;
-        /*background: linear-gradient(0deg, #00EFEF 40%, #4FDD51 100%);*/
-
-        background: linear-gradient(269deg, #00EFEF 40%, #4FDD51 100%);
+        background: #aedac7;
     }
     main{
         margin-top: 4rem;
