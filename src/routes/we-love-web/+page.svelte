a52f99c4d8f7571cf27a1aa705db1a68a052c10f
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-24 13:36:12 +0100

    I added newsletters with the news that i have beenr eading

--- src/routes/we-love-web/+page.svelte
+++ src/routes/we-love-web/+page.svelte
@@ -1,7 +1,101 @@
 <script>
 
 </script>
+<section class="general-styling-section ">
+<h1>nieuwsbrieven gelezen</h1>
 
+
+
+
+<p>
+
+    nieuws 21 november <a href="https://javascriptweekly.com/issues/714">javascirpt weekly </a> ik las een artikel over 
+    <a href="https://www.telerik.com/blogs/mapping-promises-javascript"> hier word uitgelegt hoe je een promisce kan gebruiken enw elke 3 manieren er zijn je kan een forach loop gebruiken of een rpomise.all of de  cleaner solution met p-map
+        waarin de foreach loop voor sequential operations is en promise.all voor parallel dus als je meerdere http requests hebt en p-map voor als je een module hebt en hiervoor meot je npm isntall doen p-map doen 
+       
+</p>
+
+
+
+
+
+
+<p>
+
+    nieuws 20 november <a href="https://css-weekly.com/issue-599/">Css weekly</a> ik las een artikel over <a href="https://css-tricks.com/come-to-the-light-dark-side/?utm_source=CSS-Weekly&utm_campaign=Issue-599&utm_medium=web">
+       dark en light mode </a> hoe je die kan toepassen en kan aanpassen gebaseerd op de os isntellingen van de gebruikers
+
+  
+
+    <a href="https://css-tricks.com/popping-comments-with-css-anchor-positioning-and-view-driven-animations/?utm_source=CSS-Weekly&utm_campaign=Issue-599&utm_medium=web#aa-the-foundation">
+        een aritkel</a> over css anchor positioning en view driven animations hoe je die kan toepassen 
+
+        <a href="https://ishadeed.com/article/overflow-clip/?utm_source=CSS-Weekly&utm_campaign=Issue-599&utm_medium=web">overflow uitleg</a>
+        hiern word uitgelegt hoe je overflow kan gebruiken en zit er een conventer in waarin je de code kan aanpassen en het resultaat kan zien
+
+        
+
+</p>
+
+<p>
+
+    nieuws 14 november <a href="https://javascriptweekly.com/issues/713">javascirpt weekly </a> ik las een artikel over 
+    <a href="https://www.trevorlasn.com/blog/import-attributes-in-javascript">  import-attributes-in-javascript </a>
+    daarin stond dat de server een mogelijke error kan geven als het niet goed gedefineerd is 
+       
+</p>
+<p>
+
+    nieuws 13 november <a href="https://frontendfoc.us/issues/668">frontend focus</a> ik las een artikel over <a href="https://medienbaecker.com/articles/focus-outlines">focus outlines </a> hoe je die accesible kan maken en ook mooi op
+    je website door de focus outline minimaal 2px wijd te maken en de kleur moet een raito hebben van 3:1
+
+    <a href="https://frontendmasters.com/blog/scoped-scroll-timelines/">een aritkel over scoped scroll timelines</a> in dit artikel word uitgelegt hoe je naar beneden scrolt met je muis en intussen een animatie van 0 naar 100 gaat 
+
+</p>
+
+<p>
+    niuews 7 november  <a href="https://javascriptweekly.com/issues/712">javascriptweekly</a> hierin las ik dit artikel <a href="https://web.dev/articles/top-cwv">over performance hoe je dit kan verbeteren</a>
+
+
+</p>
+
+
+<p>
+
+    nieuws 6 november <a href="https://frontendfoc.us/issues/667">frontend focus</a> ik las een artikel over <a href="https://www.joshwcomeau.com/css/container-queries-introduction/">container-queries-introduction </a>
+    
+    hoe je die kan gebruiken en dat je op kleine delen van een website styling kan doen
+
+    verder heb ik in dit artikel een subartikel gelezen over px en rems waarom je dat beter niet kan doen voor je media queries omdat mensen die hu default font increasen omdat 
+    ze het niet kunnen zien krijgen dan een website te zien waar er mogelijk problemen zijn met de layout en misschien ook wel met overflow en viewport waardoor de website niet heel toegankelijk is voor mensen die hun lettertype groot hebben
+
+    Rem is relatief  als het gaat om de font size waardoor het dus niet uitmaakt als een gebruiker in chrome instelt dat het font size groot moet zijn omdat je met rem de font mee laat groeien
+
+     <a href="https://www.joshwcomeau.com/css/surprising-truth-about-pixels-and-accessibility/">link naar het artikel</a>
+
+     <a href="https://evilmartians.com/chronicles/html-best-practices-for-login-and-signup-forms">een artikel over forms</a> In dit artikel worden tips en tricks gegeven hoe je een form kan maken 
+
+     <a href="https://frontendmasters.com/blog/scroll-driven-sections/">scroll driven sections</a> hierin is er een feautre dat heet animation-timeline: view(); en daarin word er een animatie toegevoegd terwijl de gebruiker zit te lezen
+
+
+
+
+</p>
+
+<p>
+    nieuws 23 oktober <a href="https://css-weekly.com/issue-598/">css-weekly</a> ik <a href="https://www.a11y-collective.com/blog/accessible-fonts/?utm_source=CSS-Weekly&utm_campaign=Issue-598&utm_medium=web"> dat je in je font ook goed moet
+         opletten met accesibility en dat er fotns zijn voor mensen met dislexie en mensen die niet goed kunenn zien zodat het makkeljker te lezen is 
+
+         ook heb ik gelezen dat 
+         verder heb ik gelezen over dat je de cursor zelf kan aanpassen <a href="https://www.letsbuildui.dev/articles/fun-with-custom-cursors/?utm_source=CSS-Weekly&utm_campaign=Issue-598&utm_medium=web">link naar artikel</a>
+</p>
+
+
+
+
+
+
+    </section>
 
     <section class="general-styling-section ">
         <h1>we love web</h1>
